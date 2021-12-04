29c6dc236a5c009f3ed9ca399ae3773e83297ec3
green47336 <alex.green407@gmail.com>
2021-12-04 09:23:42 -0500

    first commit from local test repo

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Game from './Game'
 
 /**
  * COMPONENT
@@ -9,7 +10,7 @@ export const Home = props => {
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <Game />
     </div>
   )
 }
