28fa11d7421e8db1718f1edf689421ac4e46d98d
green47336 <alex.green407@gmail.com>
2021-12-10 16:10:09 -0500

    created contracts build export directory in platformer/client

--- truffle-config.js
+++ truffle-config.js
@@ -3,15 +3,15 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "platformer/client/contracts"),
   networks: {
     // develop: {
     //   port: 8545,
     // },
-    ganache: {
-      host: "localhost",
+    development: {
+      host: "127.0.0.1", // was "localhost"
       port: 7545,
-      network_id: "5777",
+      network_id: "*", // was "5777"
     },
   },
   compilers: {
