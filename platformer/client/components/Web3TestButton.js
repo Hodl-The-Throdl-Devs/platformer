3b832822915e7ce02b4d5308ded171899fba282a
Riviere Fougy <rivierefougy@gmail.com>
2021-12-10 23:32:15 -0500

    attempted to send tokens from specifc address to signed address via testFunc

--- platformer/client/components/Web3TestButton.js
+++ platformer/client/components/Web3TestButton.js
@@ -7,9 +7,9 @@ const Web3TestButton = (props) => {
     const contract = contracts.metaCoin;
 
     // Sending 500 Meta Coins to specific address
-    // await contract.methods
-    //   .sendCoin("0x5B1375553513625331C90dF0497a4Ae1040Bc529", 500)
-    //   .send({ from: accounts[0] });
+    await contract.methods
+      .sendCoin(accounts[0], 500)
+      .send({ from: "0x5B1375553513625331C90dF0497a4Ae1040Bc529" });
 
     // Getting balance of signed in address
     let balance = await contract.methods.getBalance(accounts[0]).call();
