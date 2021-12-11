82210b5d807ebd19e2079b55a0753c4b755802e4
Luna <flunawonder@gmail.com>
2021-12-11 10:19:38 -0500

    server-store

---
+++ platformer/server/api/products.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const {
+  models: { Product },
+} = require("../db");
+module.exports = router;
+
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll({
+      // explicitly select only the id and username fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ["id", "username"],
+    });
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.status(200).send(await product.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
