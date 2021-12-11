645bef7642cb17243bab4549c062b3cefe644aee
Riviere Fougy <rivierefougy@gmail.com>
2021-12-10 21:37:26 -0500

    reconfigured App.js so that App extends from Component

--- platformer/client/App.js
+++ platformer/client/App.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 import Web3TestButton from "./components/Web3TestButton";
 
-const App = () => {
-  const killKaboom = () => {
+class App extends Component {
+  componentDidMount = async () => {};
+
+  killKaboom() {
     console.log(`Location: ${window.location.pathname}`);
     if (window.location.pathname !== "/game") {
       document.getElementsByTagName("CANVAS")[0].remove();
       console.log("Killing Kaboom...");
     }
-  };
-  return (
-    <div onClick={killKaboom}>
-      <Web3TestButton />
-      <Navbar />
-      <Routes />
-    </div>
-  );
-};
+  }
+
+  render() {
+    return (
+      <div onClick={this.killKaboom}>
+        <Web3TestButton />
+        <Navbar />
+        <Routes />
+      </div>
+    );
+  }
+}
 
 export default App;
