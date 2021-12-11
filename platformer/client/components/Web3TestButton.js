c4743ad7c038432895276df9dcb1d8701ef70ddc
Riviere Fougy <rivierefougy@gmail.com>
2021-12-10 23:02:07 -0500

    created feature in testFunc to send meta coin from current signed in address to specific address

--- platformer/client/components/Web3TestButton.js
+++ platformer/client/components/Web3TestButton.js
@@ -5,6 +5,13 @@ const Web3TestButton = (props) => {
 
   const testFunc = async () => {
     const contract = contracts.metaCoin;
+
+    // Sending 500 Meta Coins to specific address
+    // await contract.methods
+    //   .sendCoin("0x5B1375553513625331C90dF0497a4Ae1040Bc529", 500)
+    //   .send({ from: accounts[0] });
+
+    // Getting balance of signed in address
     let balance = await contract.methods.getBalance(accounts[0]).call();
     console.log(balance);
   };
