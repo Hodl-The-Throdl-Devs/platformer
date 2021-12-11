79f7e21782bdd8797dc3b2d4fe6d1bfc41ac8cb2
Riviere Fougy <rivierefougy@gmail.com>
2021-12-11 12:01:08 -0500

    uninstalled util, stream-browserify, crypto-browserify, buffer. removed code from webpack.config.js

--- platformer/webpack.config.js
+++ platformer/webpack.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   entry: ["./client/index.js"],
-  resolve: {
-    fallback: {
-      util: require.resolve("util/"),
-      stream: require.resolve("stream-browserify"),
-      crypto: require.resolve("crypto-browserify"),
-      buffer: require.resolve("buffer")
-
-    },
-  },
   output: {
     path: __dirname,
     filename: "./public/bundle.js",
