d932c099d2c5a88bc656688da13bf5d02ff27a1c
Riviere Fougy <rivierefougy@gmail.com>
2021-12-11 12:38:58 -0500

    installed Node Polyfill Webpack Plugin, added to webpack.config.js

--- platformer/webpack.config.js
+++ platformer/webpack.config.js
@@ -1,5 +1,8 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   entry: ["./client/index.js"],
+  plugins: [new NodePolyfillPlugin()],
   output: {
     path: __dirname,
     filename: "./public/bundle.js",
