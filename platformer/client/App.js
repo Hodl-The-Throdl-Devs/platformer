eeac60f2d2a927f1b7802228963da493bcd08b9c
Riviere Fougy <rivierefougy@gmail.com>
2021-12-10 22:23:48 -0500

    implemented web3 with App.js, added componentDidMount which has use sign in with Metamask account & have address added to state

--- platformer/client/App.js
+++ platformer/client/App.js
@@ -2,10 +2,45 @@ import React, { Component } from "react";
 
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
+
+import getWeb3 from "./getWeb3";
+import MetaCoinContract from "./contracts/MetaCoin.json";
 import Web3TestButton from "./components/Web3TestButton";
 
 class App extends Component {
-  componentDidMount = async () => {};
+  state = { storageValue: 0, web3: null, accounts: null, contracts: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = MetaCoinContract.networks[networkId];
+      const metaCoin = new web3.eth.Contract(
+        MetaCoinContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState(
+        { web3, accounts, contracts: { metaCoin } }
+        // this.runExample
+      );
+      console.log(this.state);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+    }
+  };
 
   killKaboom() {
     console.log(`Location: ${window.location.pathname}`);
--- platformer/client/getWeb3.js
+++ platformer/client/getWeb3.js
@@ -1,4 +1,4 @@
-import Web3 from "web3";
+import Web3 from "web3/dist/web3.min";
 
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
