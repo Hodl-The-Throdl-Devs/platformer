5ca2cdccf29bdfef677021c2d6854d807b05ffcc
Riviere Fougy <rivierefougy@gmail.com>
2021-12-12 14:46:46 -0500

    commented out code fin product component files

--- platformer/client/components/SingleProduct.js
+++ platformer/client/components/SingleProduct.js
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 
-// import { purchase } from "../../store/purchase";
+// // import { purchase } from "../../store/purchase";
 
-import { Button, Grid, Typography, Divider, Container, AddShoppingCartIcon } from '@mui/material';
+// import { Button, Grid, Typography, Divider, Container, AddShoppingCartIcon } from '@mui/material';
 
-const SingleProduct = (props) => {
-  const dispatch = useDispatch()
-  const products = useSelector(state => state.products)
-  const { productId } = props.match.params
-  const singleProduct = products.find(product => product.id === productId * 1)
+// const SingleProduct = (props) => {
+//   const dispatch = useDispatch()
+//   const products = useSelector(state => state.products)
+//   const { productId } = props.match.params
+//   const singleProduct = products.find(product => product.id === productId * 1)
 
-//   const [input, setinput] = useState({ quantity: 1 })
+// //   const [input, setinput] = useState({ quantity: 1 })
 
-//   const handleChange = (evt) => {
-//     const target = evt.target
-//     setinput({ [target.name]: target.value })
+// //   const handleChange = (evt) => {
+// //     const target = evt.target
+// //     setinput({ [target.name]: target.value })
+// //   }
+
+//   const purchaseSingleProduct = () => {
+//     dispatch(purchase(singleProduct))
 //   }
 
-  const purchaseSingleProduct = () => {
-    dispatch(purchase(singleProduct))
-  }
+//   useEffect(() => {
+//     window.scrollTo(0, 0)
+//   }, [])
 
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+//   return (
+//       <Container sx={{ pb: 25 }}>
+//         <Grid container sx={{ mt: 3 }}>
+//           <Grid item xs={12} sm={6} sx={{ px: 4, textAlign: 'center', '@media screen and (max-width: 600px)': { p: 0 } }}>
+//             <img loading='lazy' src={singleProduct.imageURL} width='70%' />
+//           </Grid>
+//           <Grid item xs={12} sm={6} sx={{ px: 4, textAlign: 'right' }}>
+//             <Typography variant='h5'>
+//               {singleProduct.name}
+//             </Typography>
 
-  return (
-      <Container sx={{ pb: 25 }}>
-        <Grid container sx={{ mt: 3 }}>
-          <Grid item xs={12} sm={6} sx={{ px: 4, textAlign: 'center', '@media screen and (max-width: 600px)': { p: 0 } }}>
-            <img loading='lazy' src={singleProduct.imageURL} width='70%' />
-          </Grid>
-          <Grid item xs={12} sm={6} sx={{ px: 4, textAlign: 'right' }}>
-            <Typography variant='h5'>
-              {singleProduct.name}
-            </Typography>
+//             <Typography variant='body1' color="text.secondary">
+//               {singleProduct.price}
+//             </Typography>
 
-            <Typography variant='body1' color="text.secondary">
-              {singleProduct.price}
-            </Typography>
-
-            <Button onClick={purchaseSingleProduct} variant="contained" startIcon={<AddShoppingCartIcon />} sx={{ m: 1 }}>
-                Purchase
-            </Button>
+//             <Button onClick={purchaseSingleProduct} variant="contained" startIcon={<AddShoppingCartIcon />} sx={{ m: 1 }}>
+//                 Purchase
+//             </Button>
             
-            <Divider sx={{ m: 2 }} />
-          </Grid>
-        </Grid >
-      </Container>
-  );
-}
+//             <Divider sx={{ m: 2 }} />
+//           </Grid>
+//         </Grid >
+//       </Container>
+//   );
+// }
 
-export default SingleProduct;
+// export default SingleProduct;
