0558e7c5304005063e79072f39c90fca315ed942
green47336 <alex.green407@gmail.com>
2021-12-12 18:05:41 -0500

    added Rivieres changes. now able to transfer between bank and user account

--- platformer/client/App.js
+++ platformer/client/App.js
@@ -6,23 +6,20 @@ import Routes from "./Routes";
 import getWeb3 from "./getWeb3";
 import MetaCoinContract from "./contracts/MetaCoin.json";
 import Web3TestButton from "./components/Web3TestButton";
-import HDWalletProvider from "@truffle/hdwallet-provider"
+import HDWalletProvider from "@truffle/hdwallet-provider";
 
 class App extends Component {
-  state = { web3: null, accounts: null, contracts: null };
+  state = {
+    web3: null,
+    bankAccount: ["0xDf6F1f1EB89Fd31314a64D440da7F55033299837"],
+    accounts: null,
+    contracts: null,
+  };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // HD Wallet Provider Set-Up. Make sure to replace private key when making Ganache network
-      const hdwProvider = new HDWalletProvider(
-        "84f6a0a40bb1f115e5c9c1157f9389b7ac900c127ba7264189630bf6ee0568e4","HTTP://127.0.0.1:7545"
-      );
-
-      // Set provider for Web3 to HD Wallet Provider
-      web3.setProvider(hdwProvider)
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
@@ -37,6 +34,15 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       this.setState({ web3, accounts, contracts: { metaCoin } });
+
+      // HD Wallet Provider Set-Up. Make sure to replace private key when making Ganache network
+      const hdwProvider = new HDWalletProvider(
+        "84f6a0a40bb1f115e5c9c1157f9389b7ac900c127ba7264189630bf6ee0568e4",
+        "HTTP://127.0.0.1:7545"
+      );
+
+      // Set provider for Web3 to HD Wallet Provider
+      web3.setProvider(hdwProvider);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -60,6 +66,8 @@ class App extends Component {
     return (
       <div onClick={this.killKaboom}>
         <Web3TestButton
+          state={this.state}
+          bankAccount={this.state.bankAccount}
           contracts={this.state.contracts}
           accounts={this.state.accounts}
         />
