fe4a61def59799c1a61bfaf278d6a37611de8162
Riviere Fougy <rivierefougy@gmail.com>
2021-12-13 16:01:18 -0500

    getWeb3.js & Web3TestButton.js: removed commented out code

--- platformer/client/components/Web3TestButton.js
+++ platformer/client/components/Web3TestButton.js
@@ -6,12 +6,10 @@ const Web3TestButton = (props) => {
   const testFunc = async () => {
     const contract = contracts.hodlCoin;
 
-    // Sending 500 Meta Coins to specific address
     await contract.methods
       .sendCoin(accounts[0], 500)
       .send({ from: bankAccount[0] });
 
-    // Getting balance of signed in address
     let balance = await contract.methods.getBalance(accounts[0]).call();
     console.log(balance);
   };
