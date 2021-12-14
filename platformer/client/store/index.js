bdd4dd5cb029697a118a1d87eed04f5e9691fd57
Luna <flunawonder@gmail.com>
2021-12-14 02:45:44 -0500

    product store

--- platformer/client/store/index.js
+++ platformer/client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 import products from './products'
+import purchase from './purchase'
 
-const reducer = combineReducers({ auth, products })
+const reducer = combineReducers({ auth, products, purchase})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -14,3 +15,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './auth'
 export * from "./products"
+export * from "./purchase"
