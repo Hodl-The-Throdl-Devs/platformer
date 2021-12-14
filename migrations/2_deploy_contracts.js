972cda025722ee3fd09e06c0622c1fb2950dea75
Riviere Fougy <rivierefougy@gmail.com>
2021-12-14 17:02:58 -0500

    changed named of HodlCoinOZ solidity file, incorporated HCOZ to 2_deploy_contracts.js, re-compiled

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const HodlCoin = artifacts.require("HodlCoin");
+const HodlCoinOZ = artifacts.require("HodlCoinOZ");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, HodlCoin);
   deployer.deploy(HodlCoin);
+  deployer.deploy(HodlCoinOZ);
 };
