9ac13e95d5311188f1b188d97b312680ec7d38fc
Riviere Fougy <rivierefougy@gmail.com>
2021-12-13 22:35:10 -0500

    created contracts.js via store directory

---
+++ platformer/client/store/contracts.js
@@ -0,0 +1,23 @@
+const SET_CONTRACTS = "SET_CONTRACTS";
+
+const _setContracts = (contracts) => {
+  return {
+    type: SET_CONTRACTS,
+    contracts,
+  };
+};
+
+export const setContracts = () => {
+  dispatch(_setContracts(contracts));
+};
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CONTRACTS:
+      return action.contracts;
+    default:
+      return state;
+  }
+};
