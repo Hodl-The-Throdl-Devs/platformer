ac0b61296dc7179e30ce837aadb9805c87c69e01
Spencer Posner <spencer.posner11@outlook.com>
2021-12-15 10:39:12 -0500

    added send coins but have 0 balance in sender account

--- crypto-game/client/components/Account.js
+++ crypto-game/client/components/Account.js
@@ -1,45 +1,68 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import CharacterBoard from './CharacterBoard';
+import React from "react";
+import { connect } from "react-redux";
+import CharacterBoard from "./CharacterBoard";
 
 import { updateCoins } from "../store";
 
 /**
  * COMPONENT
  */
-export const Account = props => {
-  const {auth, updateCoins, web3Props} = props
+export const Account = (props) => {
+  const { auth, updateCoins, web3Props } = props;
+  const { bankAccount, contracts, accounts, deployedNetwork } = web3Props;
+  
+
+  // trying to get balance to display balance @rfougy
+  const getBalance = async() => {
+    console.log(contracts)
+    const contract = contracts.hodlCoin;
+    await contract.methods.balanceOf(accounts[0]).call().then(res=> console.log(res));
+    // return balance;
+  }
+  
+  getBalance();
+  
+  
+  const sendTokenToUser = async () => {
+    const contract = contracts.hodlCoin;
+    await contract.methods
+      .transfer(accounts[0], auth.coins)
+      .send({ from: bankAccount[0] });
+  };
 
   const convertCoins = () => {
-    console.log(`Your coins before: ${auth.coins}`)
-    auth.coins = 0
-    updateCoins(auth);
-    console.log(`Your coins after: ${auth.coins}`)
-  }
+    sendTokenToUser().then(() => {
+      auth.coins = 0;
+      updateCoins(auth);
+    });
+  };
 
   return (
     <div>
-      Account page.
-      <button onClick={convertCoins}>Convert coins to tokens!</button>
+      <div>
+        <h1>Coin Count: {auth.coins}</h1>
+        <button onClick={convertCoins}>Convert coins to tokens!</button>
+        <h1>Hodl Count: {"balance"}</h1>
+      </div>
       <CharacterBoard />
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     auth: state.auth,
-  }
-}
+    web3Props: state.web3Props,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     updateCoins: (auth) => dispatch(updateCoins(auth)),
   };
 };
 
-
-export default connect(mapState, mapDispatch)(Account)
+export default connect(mapState, mapDispatch)(Account);
