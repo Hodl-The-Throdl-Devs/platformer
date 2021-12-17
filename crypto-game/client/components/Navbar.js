17dc552d39db66324ba3d77f23052da50244492f
Riviere Fougy <rivierefougy@gmail.com>
2021-12-17 03:17:43 -0500

    general changes made

--- crypto-game/client/components/Navbar.js
+++ crypto-game/client/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
 import { logout } from "../store";
 
 // import homeButton from "assetsUI/buttons/buttonHome.png";
