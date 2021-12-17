3fc2aa942ecaf50fcf8173fadfa1cea499adca89
Riviere Fougy <rivierefougy@gmail.com>
2021-12-17 00:54:00 -0500

    installed image-webpack-loader and file-loader

--- crypto-game/client/components/Navbar.js
+++ crypto-game/client/components/Navbar.js
@@ -3,6 +3,11 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 
+import homeButton from "../../public/assetsUI/buttons/button_home.png";
+import playButton from "../../public/assetsUI/buttons/button_play.png";
+import shopButton from "../../public/assetsUI/buttons/button_shop.png";
+import accountButton from "../../public/assetsUI/buttons/button_account.png";
+
 const Navbar = ({ handleClick, isLoggedIn, auth }) => (
   <div id="navdiv">
     <h1>hodl the throdl</h1>
@@ -10,7 +15,9 @@ const Navbar = ({ handleClick, isLoggedIn, auth }) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/home">
+            <img src={homeButton} />
+          </Link>
           <Link to="/game">Game</Link>
           <Link to="/shop">Shop</Link>
           <Link to="/account">Account</Link>
